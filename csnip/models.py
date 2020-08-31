d980314eee3d73ac3aa0a11e3a7e87312324b226
Vikrant Gawde <vikrant.gawde10@gmail.com>
2020-08-31 14:18:32 -0700

    Snip Card Design

--- csnip/models.py
+++ csnip/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Snippet(models.Model):
+    title = models.CharField(max_length = 30)
+    body = models.TextField()
+    publish = models.DateTimeField(default = timezone.now)
+    created = models.DateTimeField(auto_now_add = True)
+    updated = models.DateTimeField(auto_now = True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def__str__(self):
+    return self.title
