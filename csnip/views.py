65035f0d07ff8a43d2f0365c3662cd5cfafa08f9
Vikrant Gawde <vikrant.gawde10@gmail.com>
2020-09-04 14:27:23 -0700

    Got list view template to work

--- csnip/views.py
+++ csnip/views.py
@@ -3,9 +3,9 @@ from .models import Snippet
 
 # Create your views here.
 def snippet_list(request):
-    snippets = Snippet.created.all()
+    snippets = Snippet.published.all()
     return render(request, 'csnip/snippet/list.html', {'snippets':snippets})
 
 def snippet_detail(request, year, month, day, snippet):
     snippet = get_object_or_404(Snippet, slug=snippet, created__year=year, created__month=month, created__day=day)
-    return render(request, 'csnip/snippet/detail.html', {'snippet':snippet})    
+    return render(request, 'csnip/snippet/detail.html', {'snippet':snippet})
