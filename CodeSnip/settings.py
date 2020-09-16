b85c6b84c59eeb53d0664edee89b614880c13ec7
Vikrant Gawde <vikrant.gawde10@gmail.com>
2020-09-16 13:01:24 -0700

    added bunch of registration files and logic

--- CodeSnip/settings.py
+++ CodeSnip/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-LOGIN_REDIRECT_URL = 'dashboard'
-LOGOUT_REDIRECT_URL = 'dashboard'
+LOGIN_REDIRECT_URL = 'csnip:snippet_list'
+LOGOUT_REDIRECT_URL = 'csnip:snippet_list'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
