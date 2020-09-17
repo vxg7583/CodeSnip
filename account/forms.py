224d62e7d94c33bd611fb4e462dc6685f95d8755
Vikrant Gawde <vikrant.gawde10@gmail.com>
2020-09-17 10:24:24 -0700

    registration logic done

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,20 @@
 from django import forms
+from django.contrib.auth.models import User
 
 class LoginForm(forms.Form):
     username  = forms.CharField(widget=forms.TextInput(attrs={'class' : 'usm'}))
     password = forms.CharField(widget = forms.PasswordInput(attrs={'class' : 'pass'}))
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Password',widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat Password',widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username','first_name','email')
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise form.validationError('Passwords do not match')
+        return cd['password2']
--- account/urls.py
+++ account/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('password_reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('register/', views.register, name='register'),
 
 ]
