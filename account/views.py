25e5fc906db802c4e91578740bf39662b0c6cb67
Vikrant Gawde <vikrant.gawde10@gmail.com>
2020-09-20 16:59:27 -0700

    Follower sys works great

--- account/views.py
+++ account/views.py
@@ -3,11 +3,13 @@ from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
-from .models import Profile
+from .models import Profile, Contact
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
 from django.views.decorators.http import require_POST
 from csnip.models import Snippet
+from common.decorators import ajax_required
+from django.http import JsonResponse
 
 
 # Create your views here.
@@ -81,22 +83,24 @@ def user_detail(request, username):
     user_snippets = Snippet.publishedd.filter(user=request.user)
     return render(request, 'account/user/detail.html', {'section':'people', 'user':user, 'user_snippets':user_snippets})
 
-# @require_POST
-# @login_required
-# def user_follow(request):
-#     user_id = request.POST.get('id')
-#     action = request.POST.get('action')
-#     if user_id and action:
-#         try:
-#             user = User.objects.get(id=user_id)
-#             if action == 'follow':
-#                 Contact.objects.get_or_create(user_from=request.user,
-#                                               user_to=user)
-#                 create_action(request.user, 'is following', user)
-#             else:
-#                 Contact.objects.filter(user_from=request.user,
-#                                        user_to=user).delete()
-#             return JsonResponse({'status':'ok'})
-#         except User.DoesNotExist:
-#             return JsonResponse({'status':'ko'})
-#     return JsonResponse({'status':'ko'})
+
+@ajax_required
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == 'follow':
+                Contact.objects.get_or_create(user_from=request.user,
+                                              user_to=user)
+                # create_action(request.user, 'is following', user)
+            else:
+                Contact.objects.filter(user_from=request.user,
+                                       user_to=user).delete()
+            return JsonResponse({'status':'ok'})
+        except User.DoesNotExist:
+            return JsonResponse({'status':'ko'})
+    return JsonResponse({'status':'ko'})
